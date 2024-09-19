c23934f4804f4992d3bfc1e4cf8cd60f1aa42f11
Sebastian Cordoba <juans-cordoba@javeriana.edu.co>
2024-09-18 22:15:15 -0500

    Proyecto R

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -3,33 +3,35 @@ import { Link } from 'react-router-dom';
 import './Proyectos.css'; // Asegúrate de importar el archivo CSS
 
 export default function Proyectos() {
-  const projects = [
-    { title: "Portfolio", image: "assets/portfolioImagen.svg" },
-    { title: "Proyecto 2", image: "assets/proyecto2.svg" },
-    { title: "Proyecto 3", image: "assets/proyecto3.svg" }
-  ];
+    const projects = [
+        { title: "Portfolio", image: "assets/portfolioImagen.svg", repo: 'https://github.com/SebasRubik/Portfolio' },
+        { title: "Fundamentos Multivariado", image: "assets/RpubsFundamentosMulti.png", repo: 'http://rpubs.com/Seeb444s/FundamentosAnalisisMultivariado' },
+        { title: "Portfolio", image: "assets/portfolioImagen.svg", repo: 'https://github.com/SebasRubik/Portfolio' }
+    ];
 
-  return (
-    <div className="pantalla-proyectos-p">
-      <h1 className="titulo-principal-p">Proyectos</h1>
-      <div className="contenido-proyectos-p">
-        {projects.map((project, index) => (
-          <div key={index} className="proyecto-card-p">
-            <img src={project.image} alt={project.title} className="proyecto-imagen-p" />
-            <div className="proyecto-overlay-p">
-              <h3 className="proyecto-titulo-p">{project.title}</h3>
-              <button className="proyecto-boton-p">Ver más</button>
+    return (
+        <div className="pantalla-proyectos-p">
+            <h1 className="titulo-principal-p">Proyectos</h1>
+            <div className="contenido-proyectos-p">
+                {projects.map((project, index) => (
+                    <div key={index} className="proyecto-card-p">
+                        <img src={project.image} alt={project.title} className="proyecto-imagen-p" />
+                        <div className="proyecto-overlay-p">
+                            <h3 className="proyecto-titulo-p">{project.title}</h3>
+                            <a href={project.repo} target="_blank" rel="noopener noreferrer">
+                                <button className="proyecto-boton-p">Ver más</button>
+                            </a>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-      <nav className="menu-proyectos-p">
-        {['Principal', 'Acerca de mí', 'Habilidades', 'Contáctame'].map((item, index) => (
-          <Link key={index} to={['/', "/about-me", "/habilidades", "/contactame"][index]} className="menu-item-p">
-            {item}
-          </Link>
-        ))}
-      </nav>
-    </div>
-  );
+            <nav className="menu-proyectos-p">
+                {['Principal', 'Acerca de mí', 'Habilidades', 'Contáctame'].map((item, index) => (
+                    <Link key={index} to={['/', "/about-me", "/habilidades", "/contactame"][index]} className="menu-item-p">
+                        {item}
+                    </Link>
+                ))}
+            </nav>
+        </div>
+    );
 }
